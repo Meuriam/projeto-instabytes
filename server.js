bec4bf7b6d80309ce42c5545fd405b9b59fe376b
Meuriam Assis <meuriamassis@gmail.com>
2024-11-20 21:17:22 -0300

    meu primeiro commit

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from "express";
+
+const posts = [
+    {
+      id:1,
+      descricao: "Uma foto teste",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id:2,
+      descricao: "Gato fofo dormindo",
+      imagem: "https://placekitten.com/400/200"
+    },
+    {
+      id:3,
+      descricao: "Paisagem montanhosa",
+      imagem: "https://source.unsplash.com/random/400x300/?mountain"
+    },
+    {
+      id:4,
+      descricao: "Cachorro brincando",
+      imagem: "https://source.unsplash.com/random/300x200/?dog"
+    },
+    {
+      id:5,
+      descricao: "Comida deliciosa",
+      imagem: "https://source.unsplash.com/random/200x300/?food"
+    },
+    {
+      id:6,
+      descricao: "Cidade à noite",
+      imagem: "https://source.unsplash.com/random/300x300/?city,night"
+    }
+  ];
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000,()=> {
+    console.log("servidor escutando..");
+});
+
+app.get("/posts",(req,res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorId(id){
+    return posts.findIndex((post) => {
+        return post.id ===Number(id)
+    })
+}
+
+app.get("/posts/:id",(req,res) => {
+    const index = buscarPostPorId(req.params.id)
+    res.status(200).json(posts[index]);
+});
